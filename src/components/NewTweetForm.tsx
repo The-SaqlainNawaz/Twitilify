bc1d0e97611f03035da0010f0d2e4ceeaf53ff5e
The-SaqlainNawaz <pacsaqiirr@gmail.com>
2023-06-17 17:56:55 +0500

    tweets done like in progress

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,19 +1,57 @@
 import { useSession } from "next-auth/react";
 import Button from "./Button";
 import { ProfileImage } from "./ProfileImage";
+import { FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { api } from "~/utils/api";
+
+function upateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if(textArea==null) return
+  textArea.style.height="0"
+  textArea.style.height=`${textArea.scrollHeight}px`
+}
 
 export default function NewTweetForm() {
-    const session=useSession();
+  const session=useSession();
+  if(session.status !=="authenticated") return
+  return <Form/>
+  
+}
 
+function Form(){
+  const session=useSession();
+    const [inputValue, setInputValue]=useState("")
+    const textAreaRef=useRef<HTMLTextAreaElement>()
 
-    if(session.status !=="authenticated") return
+    const createInput=api.tweet.create.useMutation({onSuccess:newTweetForm=>{
+      console.log(newTweetForm);
+      setInputValue("")
+    },})
+    
+    function handleSubmit(e:FormEvent){
+      e.preventDefault()
 
+      createInput.mutate({content:inputValue})
+    }
+
+    const inputRef=useCallback((textArea:HTMLTextAreaElement)=>{
+    upateTextAreaSize(textArea)
+    textAreaRef.current=textArea
+    },[])
+
+    useLayoutEffect(()=>{
+      upateTextAreaSize(textAreaRef.current)
+    },[inputValue])
+
+    if(session.status !=="authenticated") return null
   return (
-   <form className="flex flex-col gap-2 border-b px-4 py-2">
+   <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
     <div className="flex gap-4">
         <ProfileImage src={session.data.user.image}/>
         <textarea 
+        ref={inputRef}
         style={{height:0}}
+        value={inputValue}
+        onChange={e=>setInputValue(e.target.value)}
         className="flex-grow resize-none overflow-hidden text-lg outline-none" placeholder="What's happening?"></textarea>
     </div>
     <Button className="self-end">Tweet</Button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,38 @@
 import { type NextPage } from "next";
+import InfiniteTweetList from "~/components/InfiniteTweetList";
 import NewTweetForm from "~/components/NewTweetForm";
+import { api } from "~/utils/api";
 
 
 const Home: NextPage = () => {
   return <>
-  <header className="sticky top-0 z-10 border-b bg-white pt-2">
-    <h1 className="mb-2 px-4 text-lg font-bold">
-      Home
-    </h1>
-  </header>
+    <header className="sticky top-0 z-10 border-b bg-white pt-2">
+      <h1 className="mb-2 px-4 text-lg font-bold">
+        Home
+      </h1>
+    </header>
 
-  <NewTweetForm/>
+    <NewTweetForm />
+    <RecentTweet />
   </>
 };
+
+function RecentTweet() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery({}, {
+    getNextPageParam: (lastPage) => {
+      lastPage.nextCursor
+    }
+  })
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweet)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNextPage={tweets.fetchNextPage}
+    />
+  )
+}
 
 export default Home;
